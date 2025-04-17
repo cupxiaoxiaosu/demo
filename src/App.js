1f6f23c9b81037d30d7534daf06eb1be464217b7
su yz <cupxiaoxiaosu@gmail.com>
2025-04-17 07:02:33 +0000

    add connect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { useEffect, useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [connected, setConnected] = useState(false);
+  useEffect(() => {}, []);
+  const connect = () => {
+    window.bitkeep.suiWallet.connect();
+  };
   return (
     <div className="App">
+      <button onClick={connect}>connected</button>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
